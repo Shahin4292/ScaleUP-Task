2c0e01ec314311e2ae74061387387a4a82dfe59e
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-01-26 15:44:24 +0600

    add title bar done

--- lib/view/login_screen/login_screen.dart
+++ lib/view/login_screen/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:scale_up_task/res/assets/app_assets.dart';
+import 'package:scale_up_task/res/colors/app_colors.dart';
 
 import '../../viewModel/login_controller/login_controller.dart';
 
@@ -11,6 +12,8 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final h = MediaQuery.sizeOf(context).height;
+    final w = MediaQuery.sizeOf(context).width;
     return Scaffold(
       backgroundColor: Colors.black,
       body: Padding(
@@ -19,10 +22,31 @@ class LoginScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Image.asset(
-              AppAssets.logo,
-              height: 100,
+            Center(
+              child: Image.asset(
+                AppAssets.logo,
+                height: 195,
+                width: 136,
+              ),
             ),
+            const SizedBox(height: 20),
+            RichText(
+              textAlign: TextAlign.center,
+                text: TextSpan(children: [
+                  TextSpan(
+                      text:
+                      "Welcome!\nto ",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.normal,
+                          fontSize: 32)),
+                  TextSpan(
+                      text: "UIPtv",
+                      style: TextStyle(
+                          color: AppColor.red,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 40)),
+                ])),
           ],
         ),
       ),
