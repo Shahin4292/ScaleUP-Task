13bbbe65c5891445b6a6061a3234dc00417ea23c
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-01-26 17:54:40 +0600

    login screen almost done

--- lib/view/login_screen/login_screen.dart
+++ lib/view/login_screen/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:scale_up_task/res/assets/app_assets.dart';
 import 'package:scale_up_task/res/colors/app_colors.dart';
+import 'package:scale_up_task/res/components/text.dart';
 
 import '../../res/components/round_button.dart';
 import '../../viewModel/login_controller/login_controller.dart';
@@ -22,7 +23,7 @@ class LoginScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-          spacing: 25,
+          spacing: 20,
           children: [
             Center(
               child: Image.asset(
@@ -47,113 +48,131 @@ class LoginScreen extends StatelessWidget {
                           fontWeight: FontWeight.bold,
                           fontSize: 40)),
                 ])),
-            Container(
-              height: 60,
-              width: MediaQuery.sizeOf(context).width,
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              decoration: BoxDecoration(
-                color: Colors.black,
-                border: Border.all(color: Colors.white),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: loginController.emailController,
-                      style: TextStyle(color: Colors.white),
-                      cursorColor: Colors.grey,
-                      decoration: InputDecoration(
-                        labelText: "Mail Address",
-                        labelStyle: TextStyle(
-                            color: AppColor.grey,
-                            fontSize: 12,
-                            fontWeight: FontWeight.normal),
-                        hintText: "yourmail@gmail.com",
-                        hintStyle: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.normal),
-                        border: InputBorder.none,
-                      ),
-                    ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 20,
+              children: [
+                Container(
+                  height: 60,
+                  width: MediaQuery.sizeOf(context).width,
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(8),
                   ),
-                  Icon(
-                    Icons.mail,
-                    color: Colors.white,
-                    size: 20,
-                  ),
-                ],
-              ),
-            ),
-            // TextField(
-            //   controller: loginController.emailController,
-            //   style: const TextStyle(color: Colors.white),
-            //   decoration: InputDecoration(
-            //     hintText: 'Mail Address',
-            //     labelText: "Mail Address",
-            //     hintStyle: TextStyle(color: Colors.grey),
-            //     suffixIcon: Icon(Icons.email, color: Colors.white),
-            //     filled: true,
-            //     fillColor: Colors.grey[800],
-            //     border: OutlineInputBorder(
-            //       borderRadius: BorderRadius.circular(10.0),
-            //       borderSide: BorderSide.none,
-            //     ),
-            //   ),
-            // ),
-            Obx(
-              () => Container(
-                height: 60,
-                width: MediaQuery.sizeOf(context).width,
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  border: Border.all(color: Colors.white),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        controller: loginController.passwordController,
-                        obscureText: loginController.isPasswordHidden.value,
-                        style: TextStyle(color: Colors.white),
-                        cursorColor: Colors.grey,
-                        decoration: InputDecoration(
-                          labelText: "Password",
-                          labelStyle: TextStyle(
-                              color: AppColor.grey,
-                              fontSize: 12,
-                              fontWeight: FontWeight.normal),
-                          hintText: "********",
-                          hintStyle: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                              fontWeight: FontWeight.normal),
-                          border: InputBorder.none,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: loginController.emailController,
+                          style: TextStyle(color: Colors.white),
+                          cursorColor: Colors.grey,
+                          decoration: InputDecoration(
+                            labelText: "Mail Address",
+                            labelStyle: TextStyle(
+                                color: AppColor.grey,
+                                fontSize: 12,
+                                fontWeight: FontWeight.normal),
+                            hintText: "yourmail@gmail.com",
+                            hintStyle: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.normal),
+                            border: InputBorder.none,
+                          ),
                         ),
                       ),
-                    ),
-                    GestureDetector(
-                      onTap: loginController.togglePasswordVisibility,
-                      child: Icon(
-                        loginController.isPasswordHidden.value
-                            ? Icons.visibility_off
-                            : Icons.visibility,
+                      Icon(
+                        Icons.mail,
                         color: Colors.white,
                         size: 20,
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
+                Obx(
+                  () => Container(
+                    height: 60,
+                    width: MediaQuery.sizeOf(context).width,
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            controller: loginController.passwordController,
+                            obscureText: loginController.isPasswordHidden.value,
+                            style: TextStyle(color: Colors.white),
+                            cursorColor: Colors.grey,
+                            decoration: InputDecoration(
+                              labelText: "Password",
+                              labelStyle: TextStyle(
+                                  color: AppColor.grey,
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.normal),
+                              hintText: "********",
+                              hintStyle: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.normal),
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: loginController.togglePasswordVisibility,
+                          child: Icon(
+                            loginController.isPasswordHidden.value
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                            color: Colors.white,
+                            size: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                RoundButton(
+                  title: 'Login',
+                  onPress: loginController.login,
+                  height: 64,
+                  width: MediaQuery.sizeOf(context).width,
+                ),
+                GestureDetector(
+                  onTap: loginController.forgotPassword,
+                  child: ModifiedText(
+                    text: 'Forgot Password',
+                    color: AppColor.redShade,
+                    fontWeight: FontWeight.normal,
+                    size: 14,
+                  ),
+                ),
+              ],
             ),
-            RoundButton(
-              title: 'Login',
-              onPress: loginController.login,
-              height: 64,
-              width: MediaQuery.sizeOf(context).width,
+            Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ModifiedText(
+                    text: "Don't have an account? ",
+                    color: Colors.white,
+                    size: 14,
+                    fontWeight: FontWeight.normal),
+                GestureDetector(
+                  // onTap: loginController.navigateToSignUp,
+                  child: ModifiedText(
+                      text: 'Sign Up',
+                      color: Colors.red,
+                      size: 14,
+                      fontWeight: FontWeight.normal),
+                ),
+              ],
             ),
           ],
         ),
