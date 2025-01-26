9332d5ab8932710804dd368390f203652cdc2ebb
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-01-26 16:38:32 +0600

    password text field add done

--- lib/view/login_screen/login_screen.dart
+++ lib/view/login_screen/login_screen.dart
@@ -86,6 +86,106 @@ class LoginScreen extends StatelessWidget {
                 ],
               ),
             ),
+            // TextField(
+            //   controller: loginController.emailController,
+            //   style: const TextStyle(color: Colors.white),
+            //   decoration: InputDecoration(
+            //     hintText: 'Mail Address',
+            //     labelText: "Mail Address",
+            //     hintStyle: TextStyle(color: Colors.grey),
+            //     suffixIcon: Icon(Icons.email, color: Colors.white),
+            //     filled: true,
+            //     fillColor: Colors.grey[800],
+            //     border: OutlineInputBorder(
+            //       borderRadius: BorderRadius.circular(10.0),
+            //       borderSide: BorderSide.none,
+            //     ),
+            //   ),
+            // ),
+            Obx(
+              () => Container(
+                height: 60,
+                width: MediaQuery.sizeOf(context).width,
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  border: Border.all(color: Colors.white),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: loginController.passwordController,
+                        obscureText: loginController.isPasswordHidden.value,
+                        style: TextStyle(color: Colors.white),
+                        cursorColor: Colors.grey,
+                        decoration: InputDecoration(
+                          labelText: "Password",
+                          labelStyle: TextStyle(
+                              color: AppColor.grey,
+                              fontSize: 12,
+                              fontWeight: FontWeight.normal),
+                          hintText: "********",
+                          hintStyle: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.normal),
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: loginController.togglePasswordVisibility,
+                      child: Icon(
+                        loginController.isPasswordHidden.value
+                            ? Icons.visibility_off
+                            : Icons.visibility,
+                        color: Colors.white,
+                        size: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            // Obx(
+            //   () => Container(
+            //     height: 60,
+            //     width: MediaQuery.sizeOf(context).width,
+            //     padding: EdgeInsets.symmetric(horizontal: 20),
+            //     decoration: BoxDecoration(
+            //       color: Colors.black,
+            //       border: Border.all(color: Colors.white),
+            //       borderRadius: BorderRadius.circular(8),
+            //     ),
+            //     child: TextField(
+            //       controller: loginController.passwordController,
+            //       obscureText: loginController.isPasswordHidden.value,
+            //       style: const TextStyle(color: Colors.white),
+            //       decoration: InputDecoration(
+            //         hintText: 'Password',
+            //         hintStyle: TextStyle(color: Colors.grey),
+            //         prefixIcon: Icon(Icons.lock, color: Colors.white),
+            //         suffixIcon: IconButton(
+            //           icon: Icon(
+            //             loginController.isPasswordHidden.value
+            //                 ? Icons.visibility_off
+            //                 : Icons.visibility,
+            //             color: Colors.white,
+            //           ),
+            //           onPressed: loginController.togglePasswordVisibility,
+            //         ),
+            //         filled: true,
+            //         fillColor: Colors.grey[800],
+            //         border: OutlineInputBorder(
+            //           borderRadius: BorderRadius.circular(10.0),
+            //           borderSide: BorderSide.none,
+            //         ),
+            //       ),
+            //     ),
+            //   ),
+            // ),
           ],
         ),
       ),
