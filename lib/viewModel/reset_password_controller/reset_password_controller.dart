999b378888bb00e7ca6ddf170f197c0d8ac12c64
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-01-27 14:07:31 +0600

    controller all add done

---
+++ lib/viewModel/reset_password_controller/reset_password_controller.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ResetPasswordController extends GetxController {
+  var confirmPasswordHidden = true.obs;
+  var newPasswordHidden = true.obs;
+  final newPasswordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+  final newPasswordFocusNode = FocusNode();
+  final confirmPasswordFocusNode = FocusNode();
+  final formKey = GlobalKey<FormState>();
+
+  void newPasswordVisibility() {
+    newPasswordHidden.value = !newPasswordHidden.value;
+  }
+
+  void confirmPasswordVisibility() {
+    confirmPasswordHidden.value = !confirmPasswordHidden.value;
+  }
+
+  void resetPassword() {
+    if (formKey.currentState!.validate()) {
+      String newPassword = newPasswordController.text;
+      String confirmPassword = confirmPasswordController.text;
+      print('newPassword: $newPassword');
+      print('confirmPassword: $confirmPassword');
+      newPasswordController.clear();
+      confirmPasswordController.clear();
+      // Get.toNamed('/sign_up_screen');
+    }
+  }
+
+  void navigateToSignUp() {
+    Get.toNamed('/signup');
+  }
+
+  void forgotPassword() {
+    Get.toNamed('/forget_password');
+  }
+}
