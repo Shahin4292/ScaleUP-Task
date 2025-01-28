003efdaa02b6c7cdf66b0003fa9ff8d9e1da2b17
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-01-28 12:26:26 +0600

    continue watching movie add done

---
+++ lib/view/movie_dashboard_screen/widget/continue_watching_movie.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import '../../../model/model.dart';
+
+class ContinueWatchingItem extends StatelessWidget {
+  final Movies movies;
+
+  const ContinueWatchingItem({super.key, required this.movies});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 98,
+          width: 166,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(6),
+              image: DecorationImage(
+                  image: NetworkImage(movies.imageUrl), fit: BoxFit.cover)),
+        ),
+        Text(movies.name,style: TextStyle(color: Colors.white),),
+      ],
+    );
+  }
+}
