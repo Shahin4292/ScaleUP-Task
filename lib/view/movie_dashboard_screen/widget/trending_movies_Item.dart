b1b43efa277a1acd38fb65931649bb5158a47e27
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-01-28 12:17:46 +0600

    Tending movie add done

---
+++ lib/view/movie_dashboard_screen/widget/trending_movies_Item.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import '../../../model/model.dart';
+
+class TrendingMoviesItem extends StatelessWidget {
+  final Movies movies;
+
+  const TrendingMoviesItem({super.key, required this.movies});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 115,
+          width: 76,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(
+                  image: NetworkImage(movies.imageUrl), fit: BoxFit.cover)),
+        ),
+        Text(movies.name,style: TextStyle(color: Colors.white),),
+      ],
+    );
+  }
+}
