1d3d50d2fd1bc9d77f9a3968c1882add85ab0e3a
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-01-28 12:33:23 +0600

    recommended movie item add done

---
+++ lib/view/movie_dashboard_screen/widget/recommended_movies_Item.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import '../../../model/model.dart';
+
+class RecommendedMoviesItem extends StatelessWidget {
+  final Movies movies;
+
+  const RecommendedMoviesItem({super.key, required this.movies});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 127,
+          width: 99,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(
+                  image: NetworkImage(movies.imageUrl), fit: BoxFit.cover)),
+        ),
+        Text(movies.name,style: TextStyle(color: Colors.white),),
+      ],
+    );
+  }
+}
